d70bfdcdc89e1d5120a3c8f7232feb94df34deed
jooskim <jsk9260@gmail.com>
2016-09-30 19:47:38 -0500

    removed material components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,15 +5,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
-import { MdButtonModule } from '@angular2-material/button';
-import { MdCardModule } from '@angular2-material/card';
 
 @NgModule({
     imports: [
         BrowserModule,
-        FormsModule,
-        MdButtonModule,
-        MdCardModule
+        FormsModule
     ],
     declarations: [ AppComponent ],
     bootstrap: [ AppComponent ]
