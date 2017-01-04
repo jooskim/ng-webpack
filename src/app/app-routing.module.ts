ab03b9dfa437b63ee9148cae5f711d55a18d88c0
jooskim <jsk9260@gmail.com>
2017-01-03 18:50:36 -0600

    indentation. cleanup in tsconfig.json

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,22 +4,22 @@ import { RouterModule, Routes } from '@angular/router';
 import { TestOneComponent } from './testone/testone.component';
 
 const appRoutes: Routes = [
-  {
-    path: 'testone',
-    component: TestOneComponent
-  },{
-    path: '',
-    redirectTo: 'testone',
-    pathMatch: 'full'
-  }
+    {
+        path: 'testone',
+        component: TestOneComponent
+    },{
+        path: '',
+        redirectTo: 'testone',
+        pathMatch: 'full'
+    }
 ];
 
 @NgModule({
-  imports: [
-    RouterModule.forRoot(appRoutes)
-  ],
-  exports: [
-    RouterModule
-  ]
+    imports: [
+        RouterModule.forRoot(appRoutes)
+    ],
+    exports: [
+        RouterModule
+    ]
 })
 export class AppRoutingModule {}
